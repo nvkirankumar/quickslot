230aeca1cc9acffa6e698d3bf0b797bf5e76419c
nvkirankumar <nujellakirankumar@gmail.com>
2025-04-12 14:27:11 +0530

    Add files via upload

---
+++ QuickSlot/src/main/java/com/example/QuickSlot/QuickSlotApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickSlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickSlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickSlotApplication.class, args);
+	}
+
+}
---
+++ QuickSlot/src/test/java/com/example/QuickSlot/QuickSlotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickSlot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickSlotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
